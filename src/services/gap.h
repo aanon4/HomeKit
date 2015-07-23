13335455f910b4ec911768267991f603de30263e
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2015-07-22 18:23:34 -0700

    Shorten acceptable connection interval

--- src/services/gap.h
+++ src/services/gap.h
@@ -9,7 +9,7 @@
 #define GAP_H_
 
 #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(20, UNIT_1_25_MS) // Minimum connection interval
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS) // Maximum connection interval
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(40, UNIT_1_25_MS) // Maximum connection interval
 #define SLAVE_LATENCY                   0                               // Slave latency.
 #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(300, UNIT_10_MS)  // Connection supervisory timeout
 
